d90eb36f452953dc83a21ef021e5064f6d430274
andela-omugeri <olive.nyotu@andela.com>
2017-03-07 16:27:05 +0300

    object oriented programming

---
+++ oopconcepts.py
@@ -0,0 +1,13 @@
+class family:
+	"""i have created family called class"""
+
+	family_members=5
+	guru=family()
+	guru.family_members=5
+
+class emma(family)
+	def emma.sing(self)	
+		print "song"
+		emma.sing()
+		
+		
